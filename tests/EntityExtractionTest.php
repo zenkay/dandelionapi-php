6d716d7b3c618ac7311f0d1a9f3d22da0938371a
Gabriel Lau <gabriel@linkest.net>
2016-07-12 03:45:52 -0300

    converted runme.php tests to real testcases

--- tests/EntityExtractionTest.php
+++ tests/EntityExtractionTest.php
@@ -2,13 +2,62 @@
     namespace Dandelionapi\Test;
 
     use Dandelionapi\apis\EntityExtraction;
+    use Dandelionapi\Dandelionapi;
 
     class EntityExtractionTest extends \PHPUnit_Framework_TestCase
     {
+        private static $APPID  = '78ef40d7';
+        private static $APPKEY = '95df210306a920bd63cc2d023f152b06';
+
         public function testInizialization()
         {
             $element = new EntityExtraction();
 
             static::assertInstanceOf('Dandelionapi\apis\EntityExtraction', $element);
+        }
+
+        /**
+         * con il wrapper si imposta la configurazione solo una volta
+         * @throws \Exception
+         */
+        public function testDandelionApi()
+        {
+            $_p = [
+                'url'     => 'https://twitter.com',
+                'include' => '"types,abstract,categories,lod',
+            ];
+            $_wrapper = new Dandelionapi(
+                [
+                    'appId'  => static::$APPID,
+                    'appKey' => static::$APPKEY
+                ]
+            );
+            $_res = $_wrapper->EntityExtraction($_p);
+
+            static::assertObjectHasAttribute('annotations', $_res);
+        }
+
+        /**
+         * chiamata diretta all'api
+         *
+         * @throws \Exception
+         */
+        public function testEntityExtraction()
+        {
+            $_p = [
+                'text'    => "The doctor says an apple is better than an orange",
+                "include" => "types,abstract,categories,lod",
+                "appId"   => static::$APPID,
+                "appKey"  => static::$APPKEY,
+            ];
+            $_api = new EntityExtraction($_p);
+
+            static::assertTrue($_api->hasAppId());
+            static::assertTrue($_api->hasAppKey());
+            static::assertFalse($_api->hasToken());
+
+            $_res = $_api->run();
+
+            static::assertObjectHasAttribute('annotations', $_res);
         }
     }
